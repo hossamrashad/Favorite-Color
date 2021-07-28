991a38a6263fd7dbb6f6f1e8b9e8da81b316418b
hossamrashad <bibo2010508@yahoo.com>
2021-07-28 12:55:32 +0200

    #661a1a

--- js/plugin.js
+++ js/plugin.js
@@ -28,21 +28,58 @@ $(document).ready(function() {
         }
     });
     // copy name of color
-    $("body").on("click", ".single-item", function() {
+    $("body").on("click", ".single-item .copyColor", function() {
         // Code
-        $(".copyColor").select();
+        $(this).select();
         document.execCommand("copy");
+        //
+        $(".popup").fadeIn(500).delay(600).fadeOut(500);
+        $(".popup").css({ background: $(this).text() });
+        $(".popup .textColor").text($(this).text());
+        $(".audio").prop({ autoplay: true });
+        var audio = new Audio("../audio/1.mp3");
+        audio.play();
     });
-});
+    // input choose color 
+    $("input[name='inputColor']").on("change", function() {
+        // code
+        console.log($(this).val());
+        var inputVal = $("input[name='inputColor']").val();
+        var ElementOutput = document.getElementById("result"),
+            myAttr = document.createAttribute("data-color");
+        myAttr.value = inputVal;
+        ElementOutput.setAttributeNode(myAttr);
 
-//
-//#3498db
-//
-//
-// #f00
-//
-//
-//
-// 
-//
-//
+        // get name of color
+        $(".result .copyColor").each(function() {
+            if ($(this).is(".copyColor")) {
+                $(this).text(inputVal);
+            }
+        });
+        // background single-item
+        $(".result").each(function() {
+            if ($(this).is(".result")) {
+                $(this).css({ background: inputVal });
+                // $(this).text(inputVal);
+            }
+        });
+    });
+
+    // copy name of color
+    $("body").on("click", ".result .copyColor", function() {
+        // Code
+        $(this).select();
+        document.execCommand("copy");
+        //
+        $(".popup").fadeIn(500).delay(600).fadeOut(500);
+        $(".popup").css({ background: $(this).text() });
+        $(".popup .textColor").text($(this).text());
+        $(".audio").prop({ autoplay: true });
+        var audio = new Audio("../audio/1.mp3");
+        audio.play();
+    });
+    // $("body").on("hover", ".parentInputChooseColor", function() {
+    //     // code
+    //     $("input[name='inputColor']").css("z-index", "1");
+    // });
+});
